17d5488100033854181c638ad7eac693305e584d
Faiz Najmuddin <faiznajmuddin123@gmail.com>
2023-12-07 14:12:41 +0700

    create ArticleRepository.kt, UserRepository.kt

---
+++ app/src/main/java/com/ch2ps215/mentorheal/domain/repository/ArticleRepository.kt
@@ -0,0 +1,21 @@
+package com.ch2ps215.mentorheal.domain.repository
+
+import com.ch2ps215.mentorheal.domain.model.Article
+
+interface ArticleRepository {
+
+    suspend fun getArticles(
+        category: String?,
+        garbageCategory: String?,
+        page: Int,
+        size: Int
+    ): List<Article>
+
+    suspend fun getArticle(token: String, articleId: Int): Article
+
+    suspend fun getFavoriteArticle(token: String): List<Article>
+
+    suspend fun like(token: String, articleId: Int): Article
+
+    suspend fun unlike(token: String, articleId: Int): Article
+}
