a8e5610d484b0861ee1a08770040f7dc232e1de4
Faiz Najmuddin <faiznajmuddin123@gmail.com>
2023-12-07 14:14:09 +0700

    add mapper for entity

---
+++ app/src/main/java/com/ch2ps215/mentorheal/data/mapper/User.kt
@@ -0,0 +1,35 @@
+package com.ch2ps215.mentorheal.data.mapper
+
+import com.ch2ps215.mentorheal.data.local.entity.UserEntity
+import com.ch2ps215.mentorheal.data.remote.payload.UserResponse
+import com.ch2ps215.mentorheal.domain.model.User
+
+fun UserResponse.asModel(): User {
+    return User(
+        id = id,
+        name = name ?: "",
+        email = email,
+        photo = null,
+        token = token
+    )
+}
+
+fun UserResponse.asEntity(): UserEntity {
+    return UserEntity(
+        id = id,
+        name = name ?: "",
+        email = email,
+        photo = null,
+        token = token
+    )
+}
+
+fun UserEntity.asModel(): User {
+    return User(
+        id = id,
+        name = name,
+        email = email,
+        photo = photo,
+        token = token
+    )
+}
